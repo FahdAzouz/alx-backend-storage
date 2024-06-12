d715af284679fd0f95909af303ae1f9b0365c3f4
FahdAzouz <fahdstudies00@gmail.com>
2024-06-13 00:23:18 +0100

    task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,13 +1,19 @@
---task 10 create function
-DELIMITER //;
-CREATE FUNCTION SafeDiv (a INTEGER, b INTEGER) RETURNS INT
+-- Script that creates a function SafeDiv that divides
+-- (and returns) the first by the second number or returns
+-- 0 if the second number is equal to 0.
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT
 DETERMINISTIC
 BEGIN
-  DECLARE result FLOAT;
-  IF b == 0 THEN
-    RETURN 0;
-  END IF;
-  SET result = (a * 1.0) / b
-  RETURN result;
-END;//
-delimiter ;
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;$$
+DELIMITER ;
