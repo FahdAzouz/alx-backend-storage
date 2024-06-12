c83e080ee27a9d08111faa2f6a4a5b6211257b96
FahdAzouz <fahdstudies00@gmail.com>
2024-06-13 00:12:25 +0100

    task 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,3 @@
---Task 9 create two indexes
-CREATE INDEX idx_name_first_score ON names(name(1), score)
+-- Script that creates an index idx_name_first_score on
+-- the table names and the first letter of name and the score.
+CREATE INDEX idx_name_first_score on names(name(1), score)
